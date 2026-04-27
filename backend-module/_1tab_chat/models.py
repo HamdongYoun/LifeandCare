b53995e45ccd7084720b2920894e249760838d15
befo0309 <befo0309@gmail.com>
2026-04-28 08:10:33 +0900

    데이터 규격

--- backend-module/_1tab_chat/models.py
+++ backend-module/_1tab_chat/models.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class ChatRequest(BaseModel):
     user_input: str
      
-class ChatResponse(BaseModel):
+class ChatResponse(BaseModel): 
     stage: int
     message: str
     show_emergency_btn: bool
